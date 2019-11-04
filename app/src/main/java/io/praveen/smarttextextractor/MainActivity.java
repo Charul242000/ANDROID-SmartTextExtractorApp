3e983564c4f9ac1e83ad5bfb5d8cf24db57fd013
Praveen Thirumurugan <praveen_thirumurugan@trimble.com>
2019-11-04 16:27:59 +0530

    Dependencies Update

--- app/src/main/java/io/praveen/smarttextextractor/MainActivity.java
+++ app/src/main/java/io/praveen/smarttextextractor/MainActivity.java
@@ -2,19 +2,16 @@ package io.praveen.smarttextextractor;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.MediaStore;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.ml.vision.FirebaseVision;
@@ -55,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK) {
             if (requestCode == 1) {
                 Uri selectedImageUri = data.getData();
@@ -89,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
             text.setText("No text found!");
             return;
         }
-        StringBuilder sb = new StringBuilder("");
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < blocks.size(); i++) {
             List<FirebaseVisionText.Line> lines = blocks.get(i).getLines();
             for (int j = 0; j < lines.size(); j++) {
